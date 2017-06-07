6d26066baa23702a267e79f877327b420dc181e5
ligreens <linagreen_@hotmail.com>
2017-06-07 18:31:20 +0200

    added changes

--- views/add.concert.php
+++ views/add.concert.php
@@ -1,5 +1,5 @@
-<div class="concerts col-lg-6 col-md-6 col-sm-6 col-xs-6">
-    <h2>Add concert</h2>
+<a href="/concert">Back</a>
+<h2>Add concert</h2>
     <form action="/create" method="POST" class="form-inline">
         <p class="concert">City:</p>
         <input type="hidden" name="id" value="">
@@ -16,6 +16,8 @@
         <button type="submit" name="update" class=" delete btn btn-primary">Add
         </button>
     </form>
+
+
     <h2>Add new city</h2>
     <form action="/add.city" method="POST" class="form-inline" >
         <p class="formfield">City</p>
@@ -37,7 +39,7 @@
     </select>
         <a class="add  btn btn-primary pull-right deletebtn" href="/delete.city?id=<?= $value['id'] ?>" role="button">Delete</a>
     </form>
-</div>
+
 
 
 
